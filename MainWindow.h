962a861ed405c536292a75fef21092b10abdc1a9
kirainmoe <i@kirainmoe.com>
2021-06-27 01:31:06 +0800

    integrade MainWindow and AddGoodItemWindow

--- MainWindow.h
+++ MainWindow.h
@@ -24,9 +24,9 @@ private:
     Ui::MainWindow *ui;
 
 public slots:
-    void onSearchButtonClick();        //搜索按钮点击事件
-    void onAddButtonClick();           //添加商品按钮点击事件
-    void onDeleteButtonClick();       //删除按钮点击事件
+    void onSearchButtonClick();        // 搜索按钮点击事件
+    void onAddButtonClick();           // 添加商品按钮点击事件
+    void onDeleteButtonClick();        // 删除按钮点击事件
 };
 
 #endif // MAINWINDOW_H
